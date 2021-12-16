54e022a180a3a1ae8cf6cfed81ac599ab6a2c929
chenew <eric@ewchen.com>
2021-12-15 21:29:12 -0600

    feat: dataframe from list

--- best_songs.py
+++ best_songs.py
@@ -2,19 +2,28 @@
 import requests
 import urllib.request as ureq
 from bs4 import BeautifulSoup as bs
+import pandas as pd
 
-best_songs_url = "https://pitchfork.com/features/lists-and-guides/best-songs-2021/"
+def sort_tracks():
+    best_songs_url = "https://pitchfork.com/features/lists-and-guides/best-songs-2021/"
 
-content = ureq.urlopen(best_songs_url).read()
+    content = ureq.urlopen(best_songs_url).read()
+    soup = bs(content, "html.parser")
+    headers = soup.find_all('h2')
 
-soup = bs(content, "html.parser")
-headers = soup.find_all('h2')
+    sort_tracks.tracks = list(map(lambda h: h.text.strip(), headers))
 
-titles = list(map(lambda h: h.text.strip(), headers))
+    total_rank = len(sort_tracks.tracks)
 
-total_rank = len(titles)
+    for rank, track in enumerate(sort_tracks.tracks):
+        rank = total_rank
+        total_rank -= 1
+        print(rank, track)
 
-for rank, song in enumerate(titles):
-    rank = total_rank
-    total_rank -= 1
-    print(rank, song)
+def dataframe():
+    df = pd.DataFrame(sort_tracks.tracks)
+    df2 = pd.DataFrame(df[0].str.split(':', expand=True).values, columns=['artist', 'track'])
+    print(df2)
+
+sort_tracks()
+dataframe()
